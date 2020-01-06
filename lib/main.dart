fee87434a497bd217cbca188047b121a711659bc
rvamsikrishna <rvamsi1993@gmail.com>
2020-01-06 13:26:45 +0530

    starting point

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello world!'),
+      ),
+    );
+  }
+}
